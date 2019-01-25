3cd0b12b5a687e5a9696a32d9faa6fa492835019
Evgeny Ukhanov <mrlsd@ya.ru>
2019-01-25 16:46:35 +0200

    Config cli: added config type for generation

--- src/config.rs
+++ src/config.rs
@@ -4,6 +4,12 @@ use std::fs;
 use toml;
 
 const DEFAULT_CONFIG_FILE: &str = "config.toml";
+const DEFAULT_CONFIG_TYPE: AppConfigType = AppConfigType::Client;
+
+enum AppConfigType {
+    Node,
+    Client,
+}
 
 #[derive(Serialize)]
 pub struct AppConfig2 {
@@ -18,17 +24,27 @@ pub struct AppConfig {
 
 fn main() {
     let mut args = env::args();
-    let config_file = if args.len() > 2 {
-        args.nth(1).unwrap()
-    } else {
-        DEFAULT_CONFIG_FILE.to_string()
-    };
-
     let cfg = AppConfig {
         name: "123".to_string(),
         app: AppConfig2 {
             name: "321".to_string(),
         },
+    };
+
+    // Fetch config generation parameters
+    let (config_type, config_file) = if args.len() > 3 {
+        let config_type = match args.nth(1).unwrap().as_ref() {
+            "node" => AppConfigType::Node,
+            _ => AppConfigType::Client,
+        };
+        (config_type, args.nth(2).unwrap())
+    } else {
+        // If config type not set - get only file name
+        if args.len() > 2 {
+            (DEFAULT_CONFIG_TYPE, args.nth(1).unwrap())
+        } else {
+            (DEFAULT_CONFIG_TYPE, DEFAULT_CONFIG_FILE.to_string())
+        }
     };
     let cfg_toml = toml::to_string(&cfg).unwrap();
 
